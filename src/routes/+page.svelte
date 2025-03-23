db1350fc9ebe201866bbfbfafc8c1477533a410c
Yashal Najeeb <yashalnajeeb@gmail.com>
2025-03-22 18:01:27 -0700

    in construction

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import HardPillsBanner from "$lib/assets/hard_pills.png";
+</script>
+
+<main
+  class="w-screen h-[100lvh] items-center px-6 md:px-40 bg-[#ecb5ec] flex flex-col pt-20 md:pt-40"
+>
+  <div class="container md:w-[40rem] h-auto">
+    <img src={HardPillsBanner} alt="banner" />
+  </div>
+  <h2 class="mt-20">RETURNING SOON</h2>
+</main>
+
