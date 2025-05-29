d13eeaf1bf6dec017c866db38113cd98ad4fe150
Yashal Najeeb <yashalnajeeb@gmail.com>
2025-05-28 22:03:11 -0700

    added string

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,5 +3,6 @@ import { DATABASE_URL } from "$env/static/private";
 import postgres from "postgres";
 import * as schema from "../../drizzle/schema";
 
-const client = postgres(DATABASE_URL, { prepare: false }); // if using Supabase connection pooling
+const connectionString = `postgres://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}?options=reference%3D${process.env.SUPABASE_PROJECT_REF}`;
+const client = postgres(connectionString, { prepare: false }); // if using Supabase connection pooling
 export const db = drizzle({ client, schema });
