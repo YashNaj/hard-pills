e4cbf56cc4429a58ef6c969cf6fd40480bebb380
Yashal Najeeb <yashalnajeeb@gmail.com>
2025-08-23 21:01:21 -0700

    drizzle config

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,10 +1,11 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import { config } from "dotenv";
+import * as schema from "../../drizzle/schema";
 
 config({ path: ".env" });
 
 const client = postgres(process.env.DATABASE_URL!, { prepare: false }); // if using Supabase connection pooling
-export const db = drizzle({ client });
+export const db = drizzle({ client, schema });
 
 console.log("🔗 [DB] Database client initialized successfully");
